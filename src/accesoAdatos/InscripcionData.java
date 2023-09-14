1ff8b111ba5bc6571cc414eea59ddbb9762bdd62
NahuelRoman1990 <nahuel.90.roman@gmail.com>
2023-09-14 19:09:00 -0300

    InscripcionData
    Metodo guardar Inscripcion

--- src/accesoAdatos/InscripcionData.java
+++ src/accesoAdatos/InscripcionData.java
@@ -5,10 +5,45 @@
  */
 package accesoAdatos;
 
+import entidades.Inscripcion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author roman
  */
 public class InscripcionData {
+    private Connection con=null;
     
+    
+    public InscripcionData(){
+        this.con=Conexion.getConnection();
+    }
+    
+    
+    public void guardarInscripcion(Inscripcion insc){
+        String sql="INSERT INTO inscripcion(idAlumno, idMateria, nota) VALUES(?,?,?)";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, insc.getAlumno().getIdAlumno());
+            ps.setInt(2, insc.getMateria().getIdMateria());
+            ps.setDouble(3, insc.getNota());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                insc.setIdInscripcion(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Inscripcion Registrada");
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla de inscripcion");
+        }
+    }
 }
